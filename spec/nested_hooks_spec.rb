47c75fb496c47010503b27c3faf30476d5b03256
Mihaela Hrituleac <hrituleacmihaela@yahoo.com>
2023-02-07 19:46:48 +0000

    nested sources more complex

--- spec/nested_hooks_spec.rb
+++ spec/nested_hooks_spec.rb
@@ -1,34 +1,47 @@
 RSpec.describe "nested hooks" do
 
 
-before(:context) do
-  puts " Outer Before context"
-end
-
-before(:example) do
-  puts "Outer Before example"
-end
-
-  it "does basic math" do
-    expect(1+1).to eq(2)
+  before(:context) do
+    puts '1 K'
   end
 
-
-context "with condition A"
-
-before(:context) do
-  puts "Inter Before context"
-end
-
-before(:example) do
-  puts "Inter Before example"
-end
-
-  it "does some more basic math" do
-    expect(2+3).to eq(5)
+  before(:example) do
+    puts '3Z'
   end
 
-  it "does some more basic math" do
-    expect(5+3).to eq(8)
+  after(:example) do
+    puts '4E'
+  end
+
+  it 'can multiply' do
+    expect(3 * 7).to eq(21)
+  end
+
+  it 'can divide' do
+    expect(40 / 8).to eq(5)
+  end
+
+  context 'with some criteria' do
+    before(:context) do
+      puts '2 H'
+    end
+
+    before(:example) do
+      puts '5B'
+    end
+
+    it 'can subtract' do
+      expect(10 - 5).to eq(5)
+    end
+
+    context 'in this condition' do
+      before(:example) do
+        puts '6V'
+      end
+
+      it 'can add' do
+        expect(8 + 6).to eq(14)
+      end
+    end
   end
 end
